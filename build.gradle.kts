48fadf11eda1cd9588104247444446359ccf9c63
luizsantana <lhzsantana@gmail.com>
2026-01-30 13:33:21 -0300

    chore: update version to 0.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.leaf"
-version = (findProperty("artifactVersion") as String?) ?: "0.3.0"
+version = (findProperty("artifactVersion") as String?) ?: "0.3.1"
 
 java {
     toolchain {
